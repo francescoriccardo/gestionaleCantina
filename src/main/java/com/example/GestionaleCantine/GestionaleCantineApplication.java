799e1b8595e53934031d36fad3e6cdde3a39e6e3
francescoriccardo <francescoriccardo1988@gmail.com>
2018-07-09 17:49:53 +0200

    first commit

---
+++ src/main/java/com/example/GestionaleCantine/GestionaleCantineApplication.java
@@ -0,0 +1,12 @@
+package com.example.GestionaleCantine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleCantineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleCantineApplication.class, args);
+	}
+}
